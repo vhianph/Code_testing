aa2740c51fadfaf8c797899ae63488ff110ba773
Le Quang Vinh <lqv140541@gmail.com>
2024-12-09 20:27:58 +0700

    basic math operation

---
+++ basic_mathematic.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#Summation
+print("Addition: " ,num1 + num2)
+
+#Substraction
+print("Substraction: ", num1 - num2)
